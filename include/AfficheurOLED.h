0a33a7e6e4c93f694e690c31fb026943a592a744
Michel Dufresne <micdufresne@gmail.com>
2026-07-24 12:01:13 +0200

    1ère version

---
+++ include/AfficheurOLED.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <Arduino.h>
+#include <Wire.h>
+//message d'erreur à ignorer, ils disparaissent quand on appelle la bibliothèque
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH110X.h>
+
+class AfficheurOLED
+{
+    private:
+        static constexpr uint8_t SCREEN_WIDTH = 128;
+        static constexpr uint8_t SCREEN_HEIGHT = 64;
+        static constexpr int8_t OLED_RESET = -1;
+        static constexpr uint8_t OLED_I2C_ADDRESS = 0x3C;
+        Adafruit_SH1106G display;
+        
+
+    public:
+        AfficheurOLED();
+
+        void initialiser(uint16_t dureeRefresh);
+
+        void afficheMessageCentre(const char *message);
+        void afficheMessageCentre(const char *message, const char *format, ...);
+        void afficheDeuxLignesCentrees(const char *ligne1, const char *ligne2);
+
+        void clear();
+
+        void AfficheurOLED::afficherTexte(
+            uint8_t taille,
+            uint8_t x,
+            uint8_t y,
+            const char *texte)
+
+        
+        void setCursor(uint8_t x, uint8_t y);
+        void setTextSize(uint8_t taille);
+        void print(const char *texte);
+        void afficher();
+};
+
+/*
+afficheur.afficheMessageCentre("Mode");
+
+afficheur.afficheMessageCentre("Trappe", "%d", angle);
+
+afficheur.afficheMessageCentre("Temp", "%.1f", temperature);
+
+afficheur.afficheMessageCentre("Humidité", "%.0f %%", humidite);
+
+afficheur.afficheMessageCentre("Durée", "%02d:%02d", h, min);
+*/
---
+++ src/AfficheurOLED.cpp
@@ -0,0 +1,117 @@
+#include "AfficheurOLED.h"
+
+AfficheurOLED::AfficheurOLED()
+    : display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET)
+{
+}
+
+void AfficheurOLED::initialiser(uint16_t dureeRefresh)
+{
+    display.begin(OLED_I2C_ADDRESS, true);
+    display.clearDisplay();
+
+}
+
+void AfficheurOLED::afficheMessageCentre(const char *message,
+                                             const char *format,
+                                             ...)
+{
+    char valeur[32];
+
+    va_list args;
+    va_start(args, format);
+    vsnprintf(valeur, sizeof(valeur), format, args);
+    va_end(args);
+
+    afficheDeuxLignesCentrees(message, valeur);
+}
+
+void AfficheurOLED::afficheMessageCentre(const char *message)
+{
+    afficheDeuxLignesCentrees(message, nullptr);
+}
+
+void AfficheurOLED::afficheDeuxLignesCentrees(const char *ligne1,
+                                              const char *ligne2)
+{
+    display.clearDisplay();
+    display.setTextSize(2);
+ 
+    int16_t x1, y1;
+    uint16_t w, h;
+
+    if (ligne2 == nullptr)
+    {
+        display.getTextBounds(ligne1, 0, 0, &x1, &y1, &w, &h);
+
+        display.setCursor((display.width() - w) / 2,
+                          (display.height() - h) / 2);
+
+        display.print(ligne1);
+    }
+    else
+    {
+        display.getTextBounds(ligne1, 0, 0, &x1, &y1, &w, &h);
+        int x = (display.width() - w) / 2;
+
+        const int hauteurLigne = 16; // police 8 px × taille 2
+        const int espace = 4;
+
+        const int hauteurTotale = hauteurLigne * 2 + espace;
+        const int y1Pos = (display.height() - hauteurTotale) / 2;
+        const int y2Pos = y1Pos + hauteurLigne + espace;
+
+        display.setCursor(x, y1Pos);
+        display.print(ligne1);
+
+        display.getTextBounds(ligne2, 0, 0, &x1, &y1, &w, &h);
+        x = (display.width() - w) / 2;
+
+        display.setCursor(x, y2Pos);
+        display.print(ligne2);
+    }
+
+    display.display();
+}
+
+
+void AfficheurOLED::clear()
+{
+    display.clearDisplay();
+    display.display();
+}
+
+void AfficheurOLED::afficherTexte(
+    uint8_t taille,
+    uint8_t x,
+    uint8_t y,
+    const char *texte)
+{
+    display.clearDisplay();
+
+    display.setTextSize(taille);
+    display.setCursor(x, y);
+    display.print(texte);
+
+    display.display();
+}
+
+void AfficheurOLED::setCursor(uint8_t x, uint8_t y)
+{
+    display.setCursor(x, y);
+}
+
+void AfficheurOLED::setTextSize(uint8_t taille)
+{
+    display.setTextSize(taille);
+}
+
+void AfficheurOLED::print(const char *texte)
+{
+    display.print(texte);
+}
+
+void AfficheurOLED::afficher()
+{
+    display.display();
+}
